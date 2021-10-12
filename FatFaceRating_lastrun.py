1192945cdbbf988741fc93c544c0685e7f2ec877
Kelsie Lau <lauk20@mcmaster.ca>
2021-10-12 18:40:55 -0400

    changed conditions file

--- FatFaceRating_lastrun.py
+++ FatFaceRating_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.2.3),
-    on Tue Oct 12 04:44:37 2021
+    on Tue Oct 12 18:04:25 2021
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
